fd4fe69b1fca23ddd4b1a5ff1ab9dfdd6c4104fa
amirnarvi <amirnarvi@gmail.com>
2024-07-04 23:34:52 +0530

    Code files created

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import Index from "./components/Index";
 import BmiCalculate from "./components/BmiCalculate";
 
 export default function App() {
